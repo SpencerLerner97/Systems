10d9cf75f5af1715d1c20234bb4a0a64e02d217d
LeChosenOne <jonahwasserman97@gmail.com>
2017-09-23 12:49:42 -0400

    cleaned up unneeded files

--- Sorter.c
+++ Sorter.c
@@ -57,5 +57,96 @@ int main(int argc, char *argv[]){
   }
   Record * newHead = mergesort(head, sortbyCol);
   //PRINT TO CSV(HEAD)
+
+  /*
+  switch(colId){
+    case 0:
+      head->color = entry;
+      break;
+    case 1:
+      head->director_name = entry;
+      break;
+    case 2:
+      head->num_critic_for_reviews = atoi(entry);
+      break;
+    case 3:
+      head->duration = atoi(entry);
+      break;
+    case 4:
+      head->director_facebook_likes = atoi(entry);
+      break;
+    case 5:
+      head->actor_3_facebook_likes = atoi(entry);
+      break;
+    case 6:
+      head->actor_2_name = entry;
+      break;
+    case 7:
+      head->actor_1_facebook_likes = atoi(entry);
+      break;
+    case 8:
+      head->gross = atoi(entry);
+      break;
+    case 9:
+      head->genres = entry;
+      break;
+    case 10:
+      head->actor_1_name = entry;
+      break;
+    case 11:
+      head->movie_title = entry;
+      break;
+    case 12:
+      head->num_voted_users = atoi(entry);
+      break;
+    case 13:
+      head->cast_total_facebook_likes = atoi(entry);
+      break;
+    case 14:
+      head->actor_3_name = entry;
+      break;
+    case 15:
+      head->facenumber_in_poster = atoi(entry);
+      break;
+    case 16:
+      head->plot_keywords = entry;
+      break;
+    case 17:
+      head->movie_imdb_link = entry;
+      break;
+    case 18:
+      head->num_user_for_reviews = atoi(entry);
+      break;
+    case 19:
+      head->language = entry;
+      break;
+    case 20:
+      head->country = entry;
+      break;
+    case 21:
+      head->content_rating = entry;
+      break;
+    case 22:
+      head->budget = atoi(entry);
+      break;
+    case 23:
+      head->title_year = atoi(entry);
+      break;
+    case 24:
+      head->actor_2_facebook_likes = atoi(entry);
+      break;
+    case 25:
+      head->imdb_score = atof(entry);
+      break;
+    case 26:
+      head->aspect_ratio = atof(entry);
+      break;
+    case 27:
+      head->movie_facebook_likes = atoi(entry);
+      break;
+    default:
+      break;
+  }
+  */
   return 0;
 }
